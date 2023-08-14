c06fe2c3e25c0e6c9146593131fd5203a674cdcc
Varsha Devkar <132808362+krushna-devkar@users.noreply.github.com>
2023-08-14 22:58:48 +0530

    Update hod.component.ts

--- hod.component.ts
+++ hod.component.ts
@@ -8,39 +8,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./hod.component.css']
 })
 export class HodComponent implements OnInit{
-  
-  content: any[] = [];
-  userId : any;
-  staff : any;
+ 
   
   constructor( private router: Router, private httpServe : HttpHandlerService) { }
   ngOnInit(): void {
-    if (localStorage.length === 0) {
-        this.router.navigate(['/login']);
-      }
-
-    this.httpServe.getUser().subscribe((data) => {
-      for(let user of data){
-        this.httpServe.getLeaves().subscribe((leaves)=>{
-          for(let sLeave of leaves){
-            this.userId = sLeave.staffId;
-            if(this.userId === user.id){
-              sLeave.fName = user.firstName;
-              sLeave.lName = user.lastName;
-              this.content.push(sLeave);
-            }
-          }
-        })
-      }
-    })
-  }
-  onApprove(){
     
-  }
-
-  onReject(){
-      
-    }
+       
   
    Logout(){
      localStorage.removeItem('user');
